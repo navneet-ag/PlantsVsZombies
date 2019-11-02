5cabb03602c13aa35a224d1a0355753f2b8475a7
navneet <navneet18348@iiitd.ac.in>
2019-11-02 20:03:34 +0530

    v1.5

--- PlantsVsZombies/src/sample/MainMenuController.java
+++ PlantsVsZombies/src/sample/MainMenuController.java
@@ -1,11 +1,18 @@
 package sample;
 
 import javafx.scene.effect.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class MainMenuController {
+
+    public ImageView SoundImageView;
+    Image SoundButtonOn=new Image("sample/SoundOn.png");
+    Image SoundButtonOff=new Image("sample/SoundOff.png");
+    private static Boolean SoundOn=true;
+    private static Boolean SoundOff=false;
     public void MouseEntered( MouseEvent mouseEvent)
     {
         ImageView OptionSelected=(ImageView)mouseEvent.getSource();
@@ -27,7 +34,7 @@ public class MainMenuController {
         ImageView PlayButton=(ImageView)mouseEvent.getSource();
         Shadow shadow=new Shadow();
         shadow.setBlurType(BlurType.THREE_PASS_BOX);
-        shadow.setColor(Color.rgb(36, 51, 77));
+        shadow.setColor(Color.rgb(14, 176, 52));
         PlayButton.setEffect(shadow);
         System.out.println("PlayPressed");
     }
@@ -44,7 +51,7 @@ public class MainMenuController {
         ImageView ResumeButton=(ImageView)mouseEvent.getSource();
         Shadow shadow=new Shadow();
         shadow.setBlurType(BlurType.THREE_PASS_BOX);
-        shadow.setColor(Color.rgb(36, 51, 77));
+        shadow.setColor(Color.rgb(14, 176, 52));
         ResumeButton.setEffect(shadow);
         System.out.println("PlayPressed");
     }
@@ -61,7 +68,7 @@ public class MainMenuController {
         ImageView ChooseLevelButton=(ImageView)mouseEvent.getSource();
         Shadow shadow=new Shadow();
         shadow.setBlurType(BlurType.THREE_PASS_BOX);
-        shadow.setColor(Color.rgb(36, 51, 77));
+        shadow.setColor(Color.rgb(14, 176, 52));
         ChooseLevelButton.setEffect(shadow);
         System.out.println("PlayPressed");
     }
@@ -78,7 +85,7 @@ public class MainMenuController {
         ImageView LeadBoarderButton=(ImageView)mouseEvent.getSource();
         Shadow shadow=new Shadow();
         shadow.setBlurType(BlurType.THREE_PASS_BOX);
-        shadow.setColor(Color.rgb(36, 51, 77));
+        shadow.setColor(Color.rgb(14, 176, 52));
         LeadBoarderButton.setEffect(shadow);
         System.out.println("PlayPressed");
     }
@@ -95,7 +102,7 @@ public class MainMenuController {
         ImageView QuitButton=(ImageView)mouseEvent.getSource();
         Shadow shadow=new Shadow();
         shadow.setBlurType(BlurType.THREE_PASS_BOX);
-        shadow.setColor(Color.rgb(36, 51, 77));
+        shadow.setColor(Color.rgb(14, 176, 52));
         QuitButton.setEffect(shadow);
         System.out.println("PlayPressed");
     }
@@ -112,7 +119,7 @@ public class MainMenuController {
         ImageView SoundButton=(ImageView)mouseEvent.getSource();
         Shadow shadow=new Shadow();
         shadow.setBlurType(BlurType.THREE_PASS_BOX);
-        shadow.setColor(Color.rgb(36, 51, 77));
+        shadow.setColor(Color.rgb(14, 176, 52));
         SoundButton.setEffect(shadow);
         System.out.println("PlayPressed");
     }
@@ -121,6 +128,19 @@ public class MainMenuController {
     {
         ImageView SoundButton=(ImageView)mouseEvent.getSource();
         SoundButton.setEffect(null);
+        if(SoundOn)
+        {
+            SoundButton.setImage(SoundButtonOff);
+            SoundOff=true;
+            SoundOn=false;
+        }
+        else
+        {
+            SoundButton.setImage(SoundButtonOn);
+            SoundOff=false;
+            SoundOn=true;
+        }
+
         System.out.println("PlayReleased");
     }
 
