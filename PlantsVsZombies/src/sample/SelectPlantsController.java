94ab73b70cd6856a8b5951c8e0193b4e4817d5f8
navneet <navneet18348@iiitd.ac.in>
2019-11-03 18:56:42 +0530

    V1.11

--- PlantsVsZombies/src/sample/SelectPlantsController.java
+++ PlantsVsZombies/src/sample/SelectPlantsController.java
@@ -1,17 +1,25 @@
 package sample;
 
+import javafx.animation.PathTransition;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.effect.Bloom;
 import javafx.scene.effect.BlurType;
 import javafx.scene.effect.Shadow;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SelectPlantsController extends Controller{
@@ -53,17 +61,53 @@ public class SelectPlantsController extends Controller{
     public void OkReleased(MouseEvent mouseEvent) throws IOException {
         //Handle IOException
         //If Exception set an image game closed abruptly
+        final ImageView SelectedZombie = new ImageView();
+        Image Zombie = new Image("sample/zombie_normal.gif");
+        SelectedZombie.setImage(Zombie);
+        SelectedZombie.setFitHeight(70);
+        SelectedZombie.setFitWidth(46.6);
+        Line line=new Line(800,125,160,125);
+
+        PathTransition transtion=new PathTransition();
+        transtion.setNode(SelectedZombie);
+        transtion.setDuration(Duration.seconds(6));
+        transtion.setPath(line);
+        transtion.setCycleCount(1);
+        transtion.play();
+        final ImageView ShootPea = new ImageView();
+        Image Pea = new Image("sample/Pea.png");
+        ShootPea.setImage(Pea);
+        ShootPea.setFitHeight(15);
+        ShootPea.setFitWidth(15);
+
+        PathTransition transtionpea=new PathTransition();
+        transtionpea.setNode(ShootPea);
+        transtionpea.setDuration(Duration.seconds(8));
+        Line linepea=new Line(200,260,800,260);
+        transtionpea.setPath(linepea);
+        transtionpea.setCycleCount(10);
+        transtionpea.play();
+
+
+        final ImageView SelectedPlant = new ImageView();
+        Image PeaShooter = new Image("sample/pea_shooter.gif");
+        SelectedPlant.setImage(PeaShooter);
+        SelectedPlant.setFitHeight(50);
+        SelectedPlant.setFitWidth(35);
+        SelectedPlant.setX(165);
+        SelectedPlant.setY(245);
 
         ImageView OkButton=(ImageView)mouseEvent.getSource();
         OkButton.setEffect(null);
         Parent tableparent = FXMLLoader.load(getClass().getResource("GamePlay.fxml"));
-        Scene tablescene =new Scene(tableparent);
+
+        Group root2=new Group(tableparent,SelectedZombie,SelectedPlant,ShootPea);
+        Scene tablescene =new Scene(root2);
         Stage window=(Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
 //        Stage window=(Stage) ((Node)event.getSource()).getScene().getWindow();
         window.setScene(tablescene);
         window.show();
         System.out.println("PlayReleased");
-
     }
     public void PlantPressed( MouseEvent mouseEvent)
     {
